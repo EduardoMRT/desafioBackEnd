543c0bbc14e6d14588ebb2a6fdd47b375a6cded1
Eduardo Miguel dos Reis Teixeira <eduksoficial@gmail.com>
2024-07-21 21:59:12 -0300

    testandoBuscaPessoa

--- desafioBackEnd/src/main/java/com/eduardo/desafioBackEnd/DesafioBackEndApplication.java
+++ desafioBackEnd/src/main/java/com/eduardo/desafioBackEnd/DesafioBackEndApplication.java
@@ -1,13 +1,22 @@
 package com.eduardo.desafioBackEnd;
 
+import com.eduardo.desafioBackEnd.JPA.Pessoa.BuscaPessoa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DesafioBackEndApplication {
 
-	public static void main(String[] args) {
+	static BuscaPessoa buscaPessoa;
+
+    public DesafioBackEndApplication(BuscaPessoa buscaPessoa) {
+        DesafioBackEndApplication.buscaPessoa = buscaPessoa;
+    }
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(DesafioBackEndApplication.class, args);
+		System.out.println(buscaPessoa.buscaPessoaPorCPF("123.456.789-10").getNomeCompleto());
 	}
 
 }
